9e7d552a145013f289b31b90e2df8c056584f0b6
JCapito <joshuaecapito22@gmail.com>
2023-01-23 21:48:55 +0000

    function renderPlanner(): Fixed issue of rendering the hour rowws onto the page

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,10 +13,15 @@
 // * Persist events between refreshes of a page
 
 $(document).ready(function() {
+
+    loadPlanner();
     // * Display the current day at the top of the calender when a user opens the planner.
     let currentDate = $('#currentDay');
     var today = moment();
     currentDate.text(today.format("dddd, MMMM D YYYY"));
+
+    let events = {};
+    let hourRendered = moment();
 
     // Render and display the daily planner 
     // The rows of the planner must start with 9 and end with 5.
@@ -28,13 +33,24 @@ $(document).ready(function() {
 
         // Increment the number of rows by 1 and display it on the page
         // Increment until 'X' number of rows is displayed
+
+        
         for (i = 1; i < 10; i++) {
             let row = $("<div>").addClass("row");
+
+            let time = "";
+            if (today.isBefore(hourRow, "hour")) {
+                time = "Future"
+            } else if (today.isAfter(hourRow, "hour")) {
+                time = "Past"
+            } else {
+                time = "Present"
+            }
 
             calendar.append(row);
             row.append($("<div>").addClass("col-2 hour").text(hourRow.format("h A")));
             let timeBlock = hourRow.format("hA");
-            row.append($("<textarea>").addClass(`col-8 ${classOfHour}`).text(events[timeBlock]));
+            row.append($("<textarea>").addClass(`col-8 ${time}`).text(events[timeBlock]));
             row.append($("<button>").addClass("col-2 saveBtn").html("<i class='fas fa-save'></i>").attr("aria-label", "Save").attr("id", hourRow.format("hA")));
 
             hourRow.add(1, "hour");
@@ -44,5 +60,19 @@ $(document).ready(function() {
     };
 
     renderPlanner(today, events);
+    
+
+    function loadPlanner() {
+        let storedEvents = JSON.parse(localStorage.getItem("events"));
+        if (storedEvents) {
+            events = storedEvents;
+        } 
+    }
+
+    // Store events with local Storage
+
+    function savePlanner() {
+        localStorage.setItem("events", JSON.stringify(events));
+    }
 
 });
