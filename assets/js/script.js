7bdbd5b0c56e80b3c6b575c4c2b4caeb016dd965
JCapito <joshuaecapito22@gmail.com>
2023-01-23 21:25:17 +0000

    function renderPlanner(): Created a FOR loop to iterate through a number of times to render each row until a given value

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,4 +18,19 @@ $(document).ready(function() {
     var today = moment();
     currentDate.text(today.format("dddd, MMMM D YYYY"));
 
+    // Render and display the daily planner 
+    // The rows of the planner must start with 9 and end with 5.
+    // Initialise the starting hour
+    function renderPlanner(today, events) {
+        let hourRow = moment(today).hour(9); // This will start the rows from 9
+        let calender = $(".container");
+        calendar.empty();
+
+        // Increment the number of rows by 1 and display it on the page
+        // Increment until 'X' number of rows is displayed
+        for (i = 1; i < 10; i++) {
+            let row = $("<div>").addClass("row");
+        }
+    }
+
 });
