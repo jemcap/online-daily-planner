8cd5746f8cd03ba868d994b91fe4b397b63b8ef8
JCapito <joshuaecapito22@gmail.com>
2023-01-23 22:10:24 +0000

    function loadPlanner(): Retrieve the item stored in localStorage to be displayed and saved onto the textarea of the planner row

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,78 +1,107 @@
 // The app should:
 
 
- 
-// * Present timeblocks for standard business hours when the user scrolls down.
- 
-// * Color-code each timeblock based on past, present, and future when the timeblock is viewed.
- 
-// * Allow a user to enter an event when they click a timeblock
+
+
+
 
 // * Save the event in local storage when the save button is clicked in that timeblock.
 
 // * Persist events between refreshes of a page
 
-$(document).ready(function() {
+$(document).ready(function () {
+  
+  // * Display the current day at the top of the calender when a user opens the planner.
+  let currentDate = $("#currentDay");
+  var today = moment();
+  currentDate.text(today.format("dddd, MMMM D YYYY"));
 
-    loadPlanner();
-    // * Display the current day at the top of the calender when a user opens the planner.
-    let currentDate = $('#currentDay');
-    var today = moment();
-    currentDate.text(today.format("dddd, MMMM D YYYY"));
+  let events = {};
+  let hourRendered = moment();
 
-    let events = {};
-    let hourRendered = moment();
+  // Render and display the daily planner
+  // The rows of the planner must start with 9 and end with 5.
+  // Initialise the starting hour
+  function renderPlanner(today, events) {
+    let hourRow = moment(today).hour(9); // This will start the rows from 9
+    let planner = $(".container");
+    planner.empty();
 
-    // Render and display the daily planner 
-    // The rows of the planner must start with 9 and end with 5.
-    // Initialise the starting hour
-    function renderPlanner(today, events) {
-        let hourRow = moment(today).hour(9); // This will start the rows from 9
-        let calendar = $(".container");
-        calendar.empty();
+    // Increment the number of rows by 1 and display it on the page
+    // Increment until 'X' number of rows is displayed
 
-        // Increment the number of rows by 1 and display it on the page
-        // Increment until 'X' number of rows is displayed
+    // * Color-code each timeblock based on past, present, and future when the timeblock is viewed.
 
-        
-        for (i = 1; i < 10; i++) {
-            let row = $("<div>").addClass("row");
+    for (i = 0; i < 9; i++) {
+      let row = $("<div>").addClass("row");
 
-            let time = "";
-            if (today.isBefore(hourRow, "hour")) {
-                time = "Future"
-            } else if (today.isAfter(hourRow, "hour")) {
-                time = "Past"
-            } else {
-                time = "Present"
-            }
+      let time = "";
+      if (today.isBefore(hourRow, "hour")) {
+        time = "Future";
+      } else if (today.isAfter(hourRow, "hour")) {
+        time = "Past";
+      } else {
+        time = "Present";
+      }
 
-            calendar.append(row);
-            row.append($("<div>").addClass("col-2 hour").text(hourRow.format("h A")));
-            let timeBlock = hourRow.format("hA");
-            row.append($("<textarea>").addClass(`col-8 ${time}`).text(events[timeBlock]));
-            row.append($("<button>").addClass("col-2 saveBtn").html("<i class='fas fa-save'></i>").attr("aria-label", "Save").attr("id", hourRow.format("hA")));
+      // Append each row to the planner with classes from bootstrap to style the row
 
-            hourRow.add(1, "hour");
+      planner.append(row);
+      row.append($("<div>").addClass("col-2 hour").text(hourRow.format("h A")));
+      let timeBlock = hourRow.format("hA");
 
-            hourRendered = moment();
-        };
-    };
+      // * Allow a user to enter an event when they click a timeblock
+      row.append(
+        $("<textarea>").addClass(`col-8 ${time}`).text(events[timeBlock])
+      );
+      row.append(
+        $("<button>")
+          .addClass("col-2 saveBtn")
+          .html("<i class='fas fa-save'></i>")
+          .attr("aria-label", "Save")
+          .attr("id", hourRow.format("hA"))
+      );
 
+      
+
+// * Present timeblocks for standard business hours when the user scrolls down.
+// Increment the timings between each row by 1.
+
+      hourRow.add(1, "hour");
+
+      hourRendered = moment();
+    }
+  }
+
+ 
+// Load and Render the planner
+  function startPlanner() {
     renderPlanner(today, events);
-    
+  }
 
-    function loadPlanner() {
-        let storedEvents = JSON.parse(localStorage.getItem("events"));
-        if (storedEvents) {
-            events = storedEvents;
-        } 
-    }
 
-    // Store events with local Storage
+  // Load and render the events that was stored in localStorage; display the item stored in localStorage
+  function loadPlanner() {
+    let storedEvents = JSON.parse(localStorage.getItem("events"));
+    if (storedEvents) {
+      events = storedEvents;
+    };
+  };
 
-    function savePlanner() {
-        localStorage.setItem("events", JSON.stringify(events));
-    }
+  loadPlanner();
+  startPlanner();
 
+  // Store events with local Storage
+
+  function savePlanner() {
+    localStorage.setItem("events", JSON.stringify(events));
+  }
+
+  // Save the event using the save button
+
+  $(document).on("click", ".saveBtn", function(e) {
+    let plannerEvent = e.currentTarget.parentElement.children[1].value;
+    events[e.currentTarget.id] = plannerEvent;
+    savePlanner();
+  });
 });
